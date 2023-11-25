3436f319a7a3c337d29217665b725e6790a9cd57
Buzzaw91 <aleksi.ylonen2@gmail.com>
2023-11-25 15:42:50 +0200

    section4 done

---
+++ how-to-code-simple-data/section-04/orange.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname orange) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define L1 (cons "Apple" (cons "Banana" (cons "Orange" (cons "Pear" empty)))))
+;; Which of the following expessions will produce the value "Orange"?
+
+;; Answer:
+
+  (first (rest (rest L1)))
